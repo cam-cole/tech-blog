b7e5e59da2229750e8f109758c2fa1caf54e051e
cam-cole <camcole065@gmail.com>
2022-07-12 15:04:02 -0500

    finished partials feature

---
+++ __tests__/helpers.test.js
@@ -0,0 +1,14 @@
+const {format_date, format_plural} = require('../utils/helpers');
+
+test('format_date() returns a date string', () => {
+    const date = new Date('2020-03-20 16:12:03');
+
+    expect(format_date(date)).toBe('3/20/2020')
+});
+
+test('format_plural()', () => {
+    const word = 'tiger';
+    const amount = 2;
+
+    expect(format_plural(word, amount)).toBe('tigers')
+})
